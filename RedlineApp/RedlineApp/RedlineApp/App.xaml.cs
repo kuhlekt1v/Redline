366f9319eb2648db85b6a1be68a894bf9dc66dbd
Cody Sheridan <cody.sheridan@student.fairfield.edu>
2021-03-01 20:45:39 -0500

    Created base project template.

---
+++ RedlineApp/RedlineApp/RedlineApp/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿/*
+    File name: App.xaml.cs
+    Purpose: Provides initial entry into application.
+    Author: Cody Sheridan
+    Version: 1.0.0
+*/
+
+using Xamarin.Forms;
+
+namespace RedlineApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
