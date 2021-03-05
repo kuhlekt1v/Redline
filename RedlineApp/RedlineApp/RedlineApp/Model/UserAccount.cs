9a0bcafa9954c5dfab359660af121498da57ec7e
Cody Sheridan <cody.sheridan@student.fairfield.edu>
2021-03-05 18:43:32 -0500

    Initial MVVM organization with Login/Register Pages

---
+++ RedlineApp/RedlineApp/RedlineApp/Model/UserAccount.cs
@@ -0,0 +1,39 @@
+﻿/*
+    File name: UserAccount.cs
+    Purpose:   Class containing all registered 
+               users' account information.
+    Author:    Cody Sheridan
+    Version:   1.0.0
+*/
+
+using SQLite;
+using System;
+
+namespace RedlineApp.Model
+{
+    class UserAccount
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [MaxLength(2)]
+        public string MiddleInitial { get; set; }
+
+        [MaxLength(255)]
+        public string LastName { get; set; }
+
+        [MaxLength(25), Unique]
+        public string Username { get; set; }
+
+        [MaxLength(25)]
+        public string Password { get; set; }
+
+        [MaxLength(255), Unique]
+        public string Email { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+    }
+}
---
+++ RedlineApp/RedlineApp/RedlineApp/View/LoginPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+
+namespace RedlineApp
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUpLinkTapped(object sender, System.EventArgs e)
+        {
+            // Set register page as current page.
+            Application.Current.MainPage = new RegisterPage();
+        }
+
+        private void LoginButtonClicked(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Success", "Login button placeholder", "Ok");
+        }
+
+        private void HelpButtonClicked(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Success", "Forgot password?", "Ok");
+        }
+    }
+}
