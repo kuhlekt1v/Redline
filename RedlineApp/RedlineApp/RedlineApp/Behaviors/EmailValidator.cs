53cc1201ea6fa55b42ff71de405f968cd41302e1
Cody Sheridan <cody.sheridan@student.fairfield.edu>
2021-03-14 08:18:30 -0400

    Validators created - Email regex, and text-only field

---
+++ RedlineApp/RedlineApp/RedlineApp/Behaviors/EmailValidator.cs
@@ -0,0 +1,37 @@
+﻿/*
+    File name: TextValidator.cs
+    Purpose:   Behavior to ensure entry contains a
+               valid email pattern.
+    Author:    Cody Sheridan
+    Version:   1.0.0
+*/
+
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace RedlineApp.Behaviors
+{
+    class EmailValidator : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var emailPattern = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))"
+            + @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
+            bool isValid = Regex.IsMatch(e.NewTextValue, emailPattern);
+
+            ((Entry)sender).TextColor = isValid ? Color.FromHex("#000") : Color.FromHex("#ff9c96");
+        }
+    }
+}
---
+++ RedlineApp/RedlineApp/RedlineApp/Behaviors/TextValidator.cs
@@ -0,0 +1,36 @@
+﻿/*
+    File name: TextValidator.cs
+    Purpose:   Behavior to ensure entry doesn't contain
+               numeric values.
+    Author:    Cody Sheridan
+    Version:   1.0.0
+*/
+
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace RedlineApp.Behaviors
+{
+    public class TextValidator : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            Regex reg = new Regex("[0-9]");
+            bool isValid = reg.IsMatch(args.NewTextValue);
+            ((Entry)sender).TextColor = isValid ? Color.Red : Color.Default;
+        }
+    }
+}
+
