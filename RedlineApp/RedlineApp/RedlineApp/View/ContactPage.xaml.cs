9b60f5740461bc3f57bcb6f4d733c6bc9930b044
Daniel Mansilla <daniel.mansilla@student.fairfield.edu>
2021-03-21 16:42:17 -0400

    GUI Medical
    UI for medical profiles

---
+++ RedlineApp/RedlineApp/RedlineApp/View/ContactPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿/*
+    File name: ContactPage.xaml.cs
+    Purpose:   Facilitate interaction with page.
+    Author:    Daniel Mansilla
+    Version:   1.0.0
+*/
+
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace RedlineApp.View
+{
+    public partial class ContactPage : ContentPage
+    {
+        public ContactPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void NextButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new ProfilePage());
+        }
+
+        private void SkipButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
---
+++ RedlineApp/RedlineApp/RedlineApp/View/PrescriptionPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿/*
+    File name: PrescriptionPage.xaml.cs
+    Purpose:   Facilitate interaction with page.
+    Author:    Daniel Mansilla
+    Version:   1.0.0
+*/
+
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace RedlineApp.View
+{
+    public partial class PrescriptionPage : ContentPage
+    {
+        public PrescriptionPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void BackButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new AllergyPage());
+        }
+
+        private void NextButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new PreconditionPage());
+        }
+    }
+}
