5bcc7160554d45be5c9d492c09b4134c275a74d7
Cody Sheridan <cody.sheridan@student.fairfield.edu>
2021-03-21 21:16:13 -0400

    Basic logout function added.

---
+++ RedlineApp/RedlineApp/RedlineApp/View/LogoutPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿/*
+    File name: LogoutPage.xaml.cs
+    Purpose:   Provide option to logout of app.
+    Author:    Cody Sheridan
+    Version:   1.0.0
+*/
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RedlineApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LogoutPage : ContentPage
+    {
+        public LogoutPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        // Display confirmation alert
+        protected async override void OnAppearing()
+        {
+            bool response = await DisplayAlert("Logout", "Are you sure?", "Yes", "No");
+
+            if (response)
+            {
+               await Navigation.PushAsync(new LoginPage());
+            }
+            else
+            {
+                await Navigation.PushAsync(new MainPage());
+            }
+
+            base.OnAppearing();
+            
+        }
+    }
+}
