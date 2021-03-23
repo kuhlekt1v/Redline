9ce95505e55a676a2e8b51d3b721105efafaea75
Cody Sheridan <sheridanc@theleeco.com>
2021-03-23 11:55:02 -0400

    Content hiding on click, and refreshing on page appearing.

--- RedlineApp/RedlineApp/RedlineApp/View/ManageAccountPage.xaml.cs
+++ RedlineApp/RedlineApp/RedlineApp/View/ManageAccountPage.xaml.cs
@@ -4,22 +4,32 @@ using Xamarin.Forms;
 
 namespace RedlineApp.View
 {
+    // REFERENCE: https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/button
     public partial class ManageAccountPage : ContentPage
     {
+        StackLayout outerStack;
+
         public ManageAccountPage()
         {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
 
-            StackLayout outerStack = new StackLayout
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            outerStack = new StackLayout
             {
                 BackgroundColor = (Color)Application.Current.Resources["AccentColor"],
                 Children =
                 {
                     // Page title label.
-                    new Label 
-                    { 
-                        Text = "Account Settings", 
-                        Padding = new Thickness(10,20, 10, 20), 
-                        Style = (Style)Application.Current.Resources["HeaderTitleStyle"] 
+                    new Label
+                    {
+                        Text = "Account Settings",
+                        Padding = new Thickness(10,20, 10, 20),
+                        Style = (Style)Application.Current.Resources["HeaderTitleStyle"]
                     }
                 }
             };
@@ -31,29 +41,40 @@ namespace RedlineApp.View
                 Padding = 0,
                 Margin = new Thickness(0, 0, 0, -10),
                 VerticalOptions = LayoutOptions.FillAndExpand,
-                BackgroundColor = (Color)Application.Current.Resources["BackgroundColor"]
+                BackgroundColor = (Color)Application.Current.Resources["BackgroundColor"],
             };
+
+            Button hideDefaultBtn = new Button
+            {
+                Text = "Hide TEST"
+                // Define properties here
+            };
+
+            // Need to add ManageAccountViewModel
+            hideDefaultBtn.Clicked += HideDefaultCategories;
+
+            StackLayout defaultCategories = new StackLayout
+            {
+                Children =
+                {
+                    hideDefaultBtn
+                }
+            };
+
+
+
+            outerStack.Children.Add(mainContentArea);
 
             // Compose the view.
             outerStack.Children.Add(mainContentArea);
+            outerStack.Children.Add(defaultCategories);
 
             Content = outerStack;
-            InitializeComponent();
-            NavigationPage.SetHasNavigationBar(this, false);
+        }
 
-            // SAMPLE
-
-            //Content = new StackLayout
-            //{
-            //    Margin = new Thickness(20),
-            //    Children =
-            //    {
-            //        new Label { Text = "Start", BackgroundColor = Color.Gray, HorizontalOptions = LayoutOptions.Start },
-            //        new Label { Text = "Center", BackgroundColor = Color.Gray, HorizontalOptions = LayoutOptions.Center },
-            //        new Label { Text = "End", BackgroundColor = Color.Gray, HorizontalOptions = LayoutOptions.End },
-            //        new Label { Text = "Fill", BackgroundColor = Color.Gray, HorizontalOptions = LayoutOptions.Fill }
-            //    }
-            //};
+        void HideDefaultCategories(object sender, EventArgs e)
+        {
+            outerStack.Children.Clear();
         }
     }
 
