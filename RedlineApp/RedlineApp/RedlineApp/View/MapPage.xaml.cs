f5392f3435f62e27b6fd7853e34b0e53c396c821
Alodaynan <kmmtvtc@gmail.com>
2021-03-26 17:06:47 -0400

    Adding map page

--- RedlineApp/RedlineApp/RedlineApp/View/MapPage.xaml.cs
+++ RedlineApp/RedlineApp/RedlineApp/View/MapPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,29 @@ namespace RedlineApp.View
         public MapPage()
         {
             InitializeComponent();
+        }
+
+        // Adding a button for Map with Coordinate include the location and option.
+        private async void ButtonOpenWithCoordinate_Clicked(object sender, EventArgs e)
+        {
+            var location = new Location(41.40090, -73.28535);
+            var options = new MapLaunchOptions { Name = "Newtown Municipal Center" };
+            await Map.OpenAsync(location, options);
+
+        }
+
+        // Adding another button for Map with placemark include the location and option.
+        private async void ButtonOpenWithPlacemark_Clicked(object sender, EventArgs e)
+        {
+            var placemark = new Placemark
+            {
+                CountryName = "United State",
+                AdminArea = "CT",
+                Thoroughfare = "Newtown Municipal Center",
+                Locality = "Fairfield"
+            };
+            var options = new MapLaunchOptions { Name = "Newtown Municipal Center" };
+            await Map.OpenAsync(placemark, options);
         }
     }
 }
