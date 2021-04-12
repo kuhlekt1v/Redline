3cbf5ae5f532fec2d7b6987564c5d67cd472ee04
Daniel Mansilla <daniel.mansilla@student.fairfield.edu>
2021-04-12 18:01:43 -0400

    home button bug

--- RedlineApp/RedlineApp/RedlineApp/View/PreconditionPage.xaml.cs
+++ RedlineApp/RedlineApp/RedlineApp/View/PreconditionPage.xaml.cs
@@ -87,7 +87,7 @@ namespace RedlineApp.View
 
         void HomeButton_Clicked(System.Object sender, System.EventArgs e)
         {
-            Navigation.PushAsync(new MedicalInformationPage());
+            Navigation.PushAsync(new MainPage());
         }
     }
 }
